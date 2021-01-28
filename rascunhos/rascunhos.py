4551bf7d0451386b3aa43618ea74df3f4ae4efdb
Lucas-far <mizoguchia@hotmail.com>
2021-01-27 23:46:20 -0300

    files edited

--- rascunhos/rascunhos.py
+++ rascunhos/rascunhos.py
@@ -1,3 +1,15 @@
 
 
-# rascunhos escritos aqui
+# rascunhos escritos aqui.
+
+"""
+Git (/ɡɪt/)[7] is a distributed version-control system for tracking changes in any set of files, originally designed
+for coordinating work among programmers cooperating on source code during software development.[8] Its goals include
+speed, data integrity, and support for distributed, non-linear workflows (thousands of parallel branches running on
+different systems).[9][10][11] Git was created by Linus Torvalds in 2005 for development of the Linux kernel, with
+other kernel developers contributing to its initial development.[12] Since 2005, Junio Hamano has been the core
+maintainer. As with most other distributed version-control systems, and unlike most client–server systems, every Git
+directory on every computer is a full-fledged repository with complete history and full version-tracking abilities,
+independent of network access or a central server.[13] Git is free and open-source software distributed under GNU
+General Public License Version 2.
+"""
--- script_aulas_git.py
+++ script_aulas_git.py
@@ -300,3 +300,94 @@ def aula_24_parte3():
     3 - Para casos de adição de novo recurso, ao final, é recomendável usar MERGE
     4 - Em casos de adição de COMMITS, atualizando branches, é recomendável usar REBASE
     """
+
+# Configuração de um arquivo [ .gitignore ]
+def aula_25():
+    """ sobre_gitignore.py """
+
+# Sobre Git stash
+def aula_26():
+    """
+    O QUE É?
+        responsável por guardar ações que ainda não foram comitadas [ status = modified ]
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ DESCRIÇÃO ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    alteração em um arquivo
+    git status
+    git stash ------------------------------------------> criar um WIP (work in progress) de uma alteração não comitada
+              * comando funciona somente após o primeiro COMMIT
+    git status -----------------------------------------------------> após o comando [ git stash ] o histórico é zerado
+    git checkout -B master2
+    touch b.txt
+    git add .
+    git commit -m 'added: b.txt'
+    git stash apply ------------------------------------------> aplica a alteração congelada feita por um [ git stash ]
+    cat a.txt -------------------------------------------> o conteúdo escrito antes de [ git stash ], estará disponível
+    git checkout master
+    cat a.txt -------------------------------------------> o conteúdo escrito antes de [ git stash ], estará disponível
+    git stash list ----------------------------------------------------> listar as vezes em que [ git stash ] foi usado
+    git stash clear -------------------------------------------------------> limpar o histórico de uso do [ git stash ]
+
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OBS ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    - Ou seja, mesmo estando em um branch onde não ocorreu a alteração [ master2 ], a alteração foi aplicada
+    - [ git stash ] congela
+    - [ git stash apply ] descongela e aplica
+    """
+
+# Atalhos para comandos Git
+def aula_27():
+    """
+    git config --global alias.st status ------------------------------------------------------> encurtar [ git status ]
+    git config --global alias.ck checkout --------------------------------------------------> encurtar [ git checkout ]
+    git config --global alias.br branch ------------------------------------------------------> encurtar [ git branch ]
+    git config --global alias.ct commit ------------------------------------------------------> encurtar [ git commit ]
+    """
+
+# Usando tags do Git
+def aula_28():
+    """
+    FUNÇÃO:
+        - não sei dizer :(
+
+    USO
+        - git tag -a 1.0 -m 'Mensagem' ------------------------------------------------------------> criação de uma tag
+        - git push origin main --tags -----------------------------------------> enviar tag para seu repositório remoto
+        - git tag --------------------------------------------------------------------------------> listar tags criadas
+
+        COMANDOS USADOS EM CONJUNTO
+        - git tag -d versão_tag ------------------------------> deletar uma tag específica, apenas do repositório local
+        - git push origin :versão_tag ---------------------------> deletar uma tag específica do seu repositório remoto
+                                      * [ origin ] é o nome do branch remoto, podendo o seu ser, diferente
+
+    DICIONÁRIO
+        -a    = anotação
+        1.0   = versão (eu recomendo ir aumentando de .1 em .1)
+        -m    = mensagem
+
+    CRIAÇÃO MANUAL PELA IDE PYCHARM:
+        - Pycharm -> Git -> new tag
+
+    INSTRUÇÕES APÓS O EMPURRO DAS TAGS:
+        - Ir a página do repositório remoto do projeto
+        - Procurar e clicar no link [ tags ] (atualmente logo acima da tabela de arquivos)
+        - As tags disponíveis serão listadas
+    """
+
+# Usando o recurso Git: revert
+def aula_29():
+    """
+    O QUE É?
+        - uma versão alternativa do comando [ git checkout / git reset --soft ]
+        - porém, seu uso resulta na criação de um outro COMMIT
+        - segundo o professor, o histórico não é perdido, servindo como um rascunho do COMMIT anterior
+
+    Alteração em algum arquivo do seu projeto
+    git commit -am 'msg'
+    No contexto da aula, vamos supor que esse commit gera um erro relevante
+    Talvez, o melhor procedimento seja usar o recurso "revert" do Git
+    git log -------------------------------------------------------> consultar o código commit de onde aconteceu o erro
+    git revert código commit
+    git log -----------------------------------------------------> ao usar [ git revert ], o COMMIT alvo não é desfeito
+            * ao invés, outro é criado, com a alteração desfeita
+    git show último código commit ----------------------------------------------------> a alteração desfeita é mostrada
+    """
