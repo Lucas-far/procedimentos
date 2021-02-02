437a6d2f297acb86b873c73e8ef16087703daccd
Lucas-far <mizoguchia@hotmail.com>
2021-02-01 23:27:21 -0300

    files edited

---
+++ configurar_pyup.py
@@ -0,0 +1,19 @@
+
+
+"""
+Módulo: configurar_pyup.py
+"""
+
+# Instalação e utilização do Pyup
+"OBS"  # Pyup mantêm as dependências de projetos atualizadas
+def parte1():
+    """
+    1 - https://pyup.io/ -> Signup pelo Github -> dar permissão de acessar repositórios da conta
+    2 - Adicionar repositório a ser checado
+    3 - O repositório será checado e se há dependências desatualizadas, estas serão mostradas
+    4 - Além da lista de dependências desatualizadas, serão criados PULL REQUESTS para fazer sua atualização
+
+    OBS:
+        - na aula, não foi explicada a parte de configuração do arquivo [ .pyup.yml ]
+        - raiz / new / file / .pyup.yml
+    """
